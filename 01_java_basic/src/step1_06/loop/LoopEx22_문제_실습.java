c2b859998dddfed407686a83728d533b4e89d109
msi <msi@DESKTOP-N3NKFMP>
2021-03-29 15:05:06 +0900

    dsf

--- 01_java_basic/src/step1_06/loop/LoopEx22_문제_실습.java
+++ 01_java_basic/src/step1_06/loop/LoopEx22_문제_실습.java
@@ -53,59 +53,113 @@ public class LoopEx22_문제_실습 {
 		 * ###
 		 */
 		
-		for (int i = 3; i > 0 ; i --) {			
-			for (int j= 0; j < i ; j++ ) {
-				System.out.print(".");
-				
-				
-				}
-			for (int k = -1; k > i ; k -- ) {		
-				System.out.print("#");
-			}
+		for(int j = 3; j>0 ; j--) {
 		
-			System.out.println("x");
+				for(int i = j; i > 0 ; i--) {
+					System.out.print(" ");
+					}
+				for(int i = j; i < 4 ; i++) {
+					System.out.print("#");
+					}
+				System.out.println();
 		}
+				
 		
 		
+		System.out.println();
 		/*
 		 * 문제 3)
 		 * ###
 		 * ##
 		 * #
 		 */
-
+		for(int j = 3; j>0 ; j--) {
+			
+			for(int i = j; i > 0 ; i--) {
+				System.out.print("#");
+				}
+			System.out.println();
+	}
 		
+		
+		
+		
+		System.out.println();
 		/*
 		 * 문제 4)
 		 * ###
 		 *  ##
 		 *   #
 		 */
-
+		for(int i = 3 ; i > 0 ; i -- ) {
+			
+				
+			
+				for(int k = 0 ; k < 3-i ; k++) {
+					System.out.print(" ");
+				}
+				
+				for( int j = i ; j > 0 ; j-- ) {
+					System.out.print("#");				
+				}
+				System.out.println();
+				
+		}
 		
+		System.out.println();
 		/*
 		 * 문제 5)
 		 * @##
 		 * @@#
 		 * @@@
 		 */
-
 		
+		for (int i = 1 ; i < 4 ; i++ ) {
+			for( int j = 0 ; j< i ; j++ ) {
+				System.out.print("@");
+				}
+			for( int k = 3 ; k > i ; k--) {
+				System.out.print("#");;
+				}
+			System.out.println(" ");
+			}
+		System.out.println();
 		/*
 		 * 문제 6)
 		 *   #
 		 *  ###
 		 * #####
 		 */
+		
+		for(int i = 3 ; i > 0 ; i--){
+			
+				for(int j = 0 ; j < i ; j ++) {
+					System.out.print(" ");
+					
+				}
+				for(int k = 6 ; k > (2*i-1) ; k--) {
+					System.out.print("#");
+				}
+				System.out.println();
 
+		}
+		System.out.println();
 		/*
 		 * 문제 7)
 		 *  #####
 		 *   ###
 		 *    #
 		 */
-
-		
+		for(int i = 0; i < 3 ; i ++) {
+				for(int k = i ; k < i ; k++) {
+					System.out.print("@");
+				
+				for(int j = (2*k-1) ; (2*k-1) < 0 ; i--) {
+				System.out.print("#");
+				}
+				}
+				System.out.println();
+		}
 		
 	}
 }
